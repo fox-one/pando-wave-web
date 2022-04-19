056fd614beac415b43eae181cf0629d3ebbb1b87
Lyric <5h3ll3x@gmail.com>
2022-04-19 22:24:48 +0900

    chore: update dep

--- src/components/layouts/modals/AuthModal.vue
+++ src/components/layouts/modals/AuthModal.vue
@@ -1,14 +1,26 @@
 <template>
-  <f-auth-method-modal ref="authModal" :fennec="fennec" @auth="handleLogin" />
+  <f-auth-method-modal
+    ref="authModal"
+    :fennec="fennec"
+    :client-id="clientId"
+    :scope="scope"
+    :is-firesbox="false"
+    @auth="handleLogin"
+    @error="handleError"
+  />
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { EVENTS } from "@/constants";
+import { EVENTS, CONFIG } from "@/constants";
 
 @Component
 class AuthModal extends Vue {
   fennec = false;
+
+  clientId = CONFIG.MIXIN_CLIENT_ID;
+
+  scope = "PROFILE:READ ASSETS:READ";
 
   mounted() {
     setTimeout(() => {
@@ -28,6 +40,10 @@ class AuthModal extends Vue {
     } else {
       this.$utils.account.authFennec(this);
     }
+  }
+
+  handleError(err) {
+    console.log(err);
   }
 }
 export default AuthModal;

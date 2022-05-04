a5c3b114a35174d45cbbe21d3a64eebe94fb400c
Lyric <5h3ll3x@gmail.com>
2022-05-04 12:53:19 +0900

    chore: add i18n support

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,20 @@
 import en from "./lang/en-US.json";
 import zh from "./lang/zh-TW.json";
+import ja from "./lang/ja-JP.json";
+import de from "./lang/de-DE.json";
+import es from "./lang/es-ES.json";
+import fr from "./lang/fr-FR.json";
+import ko from "./lang/ko-KR.json";
 
 export default {
   fallbackLocale: "en",
   messages: {
     en,
     zh,
+    ja,
+    de,
+    es,
+    fr,
+    ko,
   },
 };
